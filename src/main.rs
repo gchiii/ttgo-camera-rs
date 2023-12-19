77c156c18c0deade32c7d8af6f43596c2f4d399a
George C. Huntington III <ghuntington@hubbell.com>
2023-12-19 15:25:57 -0500

    working better with wrapper

--- src/main.rs
+++ src/main.rs
@@ -30,90 +30,24 @@ use esp_idf_sys::camera::{self, camera_config_t__bindgen_ty_1, camera_config_t__
 // Camera Resolution: 2 Megapixel
 
 // this is from https://makeradvisor.com/esp32-ttgo-t-camera-pir-sensor-oled/
-const CAM_PIN_PWDN: ::std::os::raw::c_int = 26;   // define PWDN_GPIO_NUM -1
-const CAM_PIN_RESET: ::std::os::raw::c_int = -1; //software reset will be performed   // define RESET_GPIO_NUM -1
-const CAM_PIN_XCLK: ::std::os::raw::c_int = 4;   // define XCLK_GPIO_NUM 32
-const CAM_PIN_SIOD: ::std::os::raw::c_int = 18;   // define SIOD_GPIO_NUM 13
-const CAM_PIN_SIOC: ::std::os::raw::c_int = 23;   // define SIOC_GPIO_NUM 12
-const CAM_PIN_D7: ::std::os::raw::c_int = 36;   // define Y9_GPIO_NUM 39
-const CAM_PIN_D6: ::std::os::raw::c_int = 15;   // define Y8_GPIO_NUM 36
-const CAM_PIN_D5: ::std::os::raw::c_int = 12;   // define Y7_GPIO_NUM 23
-const CAM_PIN_D4: ::std::os::raw::c_int = 39;   // define Y6_GPIO_NUM 18
-const CAM_PIN_D3: ::std::os::raw::c_int = 35;   // define Y5_GPIO_NUM 15
-const CAM_PIN_D2: ::std::os::raw::c_int = 14;   // define Y4_GPIO_NUM 4
-const CAM_PIN_D1: ::std::os::raw::c_int = 13;   // define Y3_GPIO_NUM 14
-const CAM_PIN_D0: ::std::os::raw::c_int = 34;   // define Y2_GPIO_NUM 5
-const CAM_PIN_VSYNC: ::std::os::raw::c_int = 5;   // define VSYNC_GPIO_NUM 27
-const CAM_PIN_HREF: ::std::os::raw::c_int = 27;   // define HREF_GPIO_NUM 25
-const CAM_PIN_PCLK: ::std::os::raw::c_int = 25;   // define PCLK_GPIO_NUM 19
+// const CAM_PIN_PWDN: ::std::os::raw::c_int = 26;   // define PWDN_GPIO_NUM -1
+// const CAM_PIN_RESET: ::std::os::raw::c_int = -1; //software reset will be performed   // define RESET_GPIO_NUM -1
+// const CAM_PIN_XCLK: ::std::os::raw::c_int = 4;   // define XCLK_GPIO_NUM 32
+// const CAM_PIN_SIOD: ::std::os::raw::c_int = 18;   // define SIOD_GPIO_NUM 13
+// const CAM_PIN_SIOC: ::std::os::raw::c_int = 23;   // define SIOC_GPIO_NUM 12
+// const CAM_PIN_D7: ::std::os::raw::c_int = 36;   // define Y9_GPIO_NUM 39
+// const CAM_PIN_D6: ::std::os::raw::c_int = 15;   // define Y8_GPIO_NUM 36
+// const CAM_PIN_D5: ::std::os::raw::c_int = 12;   // define Y7_GPIO_NUM 23
+// const CAM_PIN_D4: ::std::os::raw::c_int = 39;   // define Y6_GPIO_NUM 18
+// const CAM_PIN_D3: ::std::os::raw::c_int = 35;   // define Y5_GPIO_NUM 15
+// const CAM_PIN_D2: ::std::os::raw::c_int = 14;   // define Y4_GPIO_NUM 4
+// const CAM_PIN_D1: ::std::os::raw::c_int = 13;   // define Y3_GPIO_NUM 14
+// const CAM_PIN_D0: ::std::os::raw::c_int = 34;   // define Y2_GPIO_NUM 5
+// const CAM_PIN_VSYNC: ::std::os::raw::c_int = 5;   // define VSYNC_GPIO_NUM 27
+// const CAM_PIN_HREF: ::std::os::raw::c_int = 27;   // define HREF_GPIO_NUM 25
+// const CAM_PIN_PCLK: ::std::os::raw::c_int = 25;   // define PCLK_GPIO_NUM 19
 
 
-
-fn init_camera() -> Result<(), EspError> {
-
-    let config = camera::camera_config_t {
-        pin_pwdn: CAM_PIN_PWDN,
-        pin_reset: CAM_PIN_RESET,
-        pin_xclk: CAM_PIN_XCLK,
-        __bindgen_anon_1: camera_config_t__bindgen_ty_1{
-            pin_sccb_sda: CAM_PIN_SIOD,
-        },
-        __bindgen_anon_2: camera_config_t__bindgen_ty_2{
-            pin_sccb_scl: CAM_PIN_SIOC,
-        },
-
-        pin_d7: CAM_PIN_D7,
-        pin_d6: CAM_PIN_D6,
-        pin_d5: CAM_PIN_D5,
-        pin_d4: CAM_PIN_D4,
-        pin_d3: CAM_PIN_D3,
-        pin_d2: CAM_PIN_D2,
-        pin_d1: CAM_PIN_D1,
-        pin_d0: CAM_PIN_D0,
-        pin_vsync: CAM_PIN_VSYNC,
-        pin_href: CAM_PIN_HREF,
-        pin_pclk: CAM_PIN_PCLK,
-
-        xclk_freq_hz: 20000000,
-        ledc_timer: camera::ledc_timer_t_LEDC_TIMER_0,
-        ledc_channel: camera::ledc_channel_t_LEDC_CHANNEL_0,
-        pixel_format: camera::pixformat_t_PIXFORMAT_JPEG,
-        frame_size: camera::framesize_t_FRAMESIZE_QVGA,
-        jpeg_quality: 12,
-        fb_count: 1,
-        fb_location: camera::camera_fb_location_t_CAMERA_FB_IN_PSRAM,
-        grab_mode: camera::camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
-        sccb_i2c_port: -1,
-    };
-
-    esp_idf_sys::esp!(unsafe { camera::esp_camera_init(&config) })
-}
-
-
-
-fn alt_take_pic() -> Result<DynamicImage, ImageError>{
-    let fb = unsafe { camera::esp_camera_fb_get() };
-
-    let mut buf: *mut u8 = unsafe { libc::malloc(std::mem::size_of::<u8>()) as *mut u8 };
-    let mut buf_len = 0;
-
-    unsafe { camera::frame2bmp(fb, &mut buf, &mut buf_len) } ;
-
-    let photo = unsafe { std::slice::from_raw_parts_mut(buf, buf_len) };
-
-    // let photo = unsafe { std::slice::from_raw_parts((*fb).buf, (*fb).len) };
-
-    println!("Read");
-    let img = ImageReader::new(Cursor::new(photo))
-        .with_guessed_format()?
-        .decode()?;
-    unsafe {
-        libc::free(buf as *mut libc::c_void);
-        camera::esp_camera_fb_return(fb);
-    }
-
-    Ok(img)
-}
 
 fn image_to_ascii(image: DynamicImage) {
     let resolution = 8;
@@ -263,7 +197,7 @@ fn main() {
         Some(cam_sda),
         Some(cam_scl),
     );
-    let camera = match p_camera {
+    let camera: Camera<'_> = match p_camera {
         Ok(c) => c,
         Err(e) => {
             log::error!("{}", e);
@@ -271,55 +205,35 @@ fn main() {
         },
     };
     let _sensor = camera.sensor();
-    // if let Err(e) = _sensor.set_pixformat(camera::pixformat_t_PIXFORMAT_JPEG) {
+    // if let Err(e) = _sensor.set_pixformat(camera::pixformat_t_PIXFORMAT_GRAYSCALE) {
     //     log::error!("{}", e);
     // }
     if let Err(e) = _sensor.init_status() {
         log::error!("{}", e);
     }
-    // if let Err(e) = _sensor. {
+    // if let Err(e) = _sensor.set_framesize(framesize) {
     //     log::error!("{}", e);
     // }
     loop {
-        println!("Read");
-        if let Ok(pic) = alt_take_pic() {
-            // Move to the top left
-            print!("{esc}[1;1H", esc = 27 as char);
-            // pic.
-            image_to_ascii(pic);
+        if let Some(fb) = camera.get_framebuffer() {
+            match fb.data_as_bmp() {
+                Ok(pic) => {
+                    if let Ok(img_reader) = ImageReader::new(Cursor::new(pic)).with_guessed_format() {
+                        match img_reader.decode() {
+                            Ok(image) => {
+                                // Move to the top left
+                                print!("{esc}[1;1H", esc = 27 as char);
+                                // pic.
+                                image_to_ascii(image);
+                            },
+                            Err(e) => log::error!("{}", e),
+                        }
+                    }
+                }
+                Err(e)=> log::error!("{}", e),
+            }
         }
-        // if let Some(fb) = camera.get_framebuffer() {
-        //     match fb.data_as_jpeg(20) {
-        //         Ok(pic) => {
-        //             if let Ok(img_reader) = ImageReader::new(Cursor::new(pic)).with_guessed_format() {
-        //                 match img_reader.decode() {
-        //                     Ok(image) => image_to_ascii(image),
-        //                     Err(e) => log::error!("{}", e),
-        //                 }
-        //             }
-        //         }
-        //         Err(e)=> log::error!("{}", e),
-        //     }
-        // }
         FreeRtos::delay_ms(2000);
     }
-
-    // if init_camera().is_ok() {
-    //     // Reset terminal
-    //     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    //     while let Ok(pic) = alt_take_pic() {
-    //         // Move to the top left
-    //         print!("{esc}[1;1H", esc = 27 as char);
-    //         // pic.
-    //         image_to_ascii(pic);
-    //         FreeRtos::delay_ms(2000);
-    //     }
-    // } else {
-    //     loop {
-    //         FreeRtos::delay_ms(2000);
-    //         log::info!("Hello, world!");
-    //     }
-    // }
-
 }
 
